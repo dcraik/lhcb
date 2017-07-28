d94c9d769d0665e98d2d31f6015c5140a2e42412
Daniel Craik <daniel.charles.craik@cern.ch>
2017-07-28 21:24:01 +0200

    add skimming scripts

---
+++ jets/skimTuples/skimSV.C
@@ -0,0 +1,15 @@
+{
+	TFile* f = TFile::Open("/tmp/dcraik/for_yandex_data.root");
+	TTree* t = dynamic_cast<TTree*>(f->Get("T"));
+
+//	double JetDijetSVDec;
+//
+//	t->SetBrancheAddress("JetDijetSVDec", &JetDijetSVDec);
+//
+//	int n = t->GetEntries();
+
+	TFile* fout = new TFile("/tmp/dcraik/for_yandex_data_SV.root","RECREATE");
+	TTree* tout = t->CopyTree("JetDijetSVDec>0");
+	tout->Write();
+	fout->Close();
+}
