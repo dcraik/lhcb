e8c627f62d814f99af00f0f48db0755ec406f5ed
Daniel Craik <daniel.charles.craik@cern.ch>
2018-09-26 13:58:29 -0400

    added first pass of comments removal to deal with \input{acknowledgements_intro} bug

--- prepPaper/prepPaper.sh
+++ prepPaper/prepPaper.sh
@@ -8,9 +8,23 @@ cd build
 #make bbl file
 pdflatex main; bibtex main;
 
-#do inputs
 cp main.tex main.step0.tex
 
+#do comments and blank lines ### first pass
+# - delete all comment lines (% preceeded by on whitespace)
+# - delete all trailing comments (characters after % unless preceeded by \)
+# - remove multiple blank lines
+vim -E -s main.tex <<-EOF
+	:g/^\\s*%/d
+	:g/%/s/\\(.*[^\\\\]\\)%.*/\\1/g
+	:%s/\\s\\+$//e
+	:%s/\n\\{3,}/\r\r/e
+	:wq
+EOF
+
+cp main.tex main.step1.tex
+
+#do inputs
 while
 	cat main.tex | grep -E "\\input{" | grep -v "^\s*%" > inputs.txt
 	python inputs.py
@@ -20,6 +34,7 @@ while
 do
 	continue
 done
+cp main.tex main.step2.tex
 
 #do lhcb logo and bibliography
 vim -E -s main.tex <<-EOF
@@ -34,7 +49,7 @@ vim -E -s main.tex <<-EOF
 EOF
 mv figs/lhcb-logo.pdf .
 
-cp main.tex main.step1.tex
+cp main.tex main.step3.tex
 #do comments and blank lines
 # - delete all comment lines (% preceeded by on whitespace)
 # - delete all trailing comments (characters after % unless preceeded by \)
@@ -47,7 +62,7 @@ vim -E -s main.tex <<-EOF
 	:wq
 EOF
 
-cp main.tex main.step2.tex
+cp main.tex main.step4.tex
 #do figures
 cat main.tex | grep -E "begin{figure|includegraphics" | grep -v "^\s*%" > figs.txt
 vim -E -s figs.txt <<-EOF
@@ -61,10 +76,10 @@ chmod +x figs.sh
 # tar it up
 mkdir -p tar
 cp main.tex fig*.pdf lhcb-logo.pdf *.xml *.sty tar/
-tar -zcf ../PAPER.tar.gz tar/*
+cd tar/
+tar -zcf ../../PAPER.tar.gz *
 
 # make the pdf
-cd tar
 pdflatex main
 pdflatex main
 cp main.pdf ../../PAPER.pdf
