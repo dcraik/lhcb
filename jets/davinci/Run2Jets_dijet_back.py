066bb698201f81ba41722b982acf6a9d72959b74
Daniel Craik <daniel.charles.craik@cern.ch>
2019-04-01 04:42:36 +0200

    updated backwards-tagged dijet scripts

---
+++ jets/davinci/Run2Jets_dijet_back.py
@@ -0,0 +1,233 @@
+#!/bin/env python
+import sys
+sys.path.append('.')
+
+from PhysConf.Filters import LoKi_Filters
+fltrs = LoKi_Filters (
+####DiJet*
+#    STRIP_Code = """
+#   HLT_PASS_RE ( 'StrippingHltQEEJetsDiJet.*LineDecision'    )
+#   """
+#    )
+###DiJet{,HighPt,LowPt}
+    STRIP_Code = """
+   HLT_PASS_RE ( 'StrippingHltQEEJetsDiJetLineDecision' )
+   | HLT_PASS_RE ( 'StrippingHltQEEJetsDiJetHighPtLineDecision' )
+   | HLT_PASS_RE ( 'StrippingHltQEEJetsDiJetLowPtLineDecision' )
+   """
+    )
+####DiJetSV*
+#    STRIP_Code = """
+#   HLT_PASS_RE ( 'StrippingHltQEEJetsDiJetSV.*LineDecision' )
+#   """
+#    )
+####DiJet
+#    STRIP_Code = """
+#   HLT_PASS_RE ( 'StrippingHltQEEJetsDiJetLineDecision'    )
+#   """
+#    )
+
+
+# Data type configuration.
+from GaudiKernel import SystemOfUnits as Units
+##Type     = 'MC'
+JetPtMin = 10 * Units.GeV
+
+
+## Data.
+#from GaudiConf import IOHelper
+#IOHelper('ROOT').inputFiles(['/eos/lhcb/grid/prod/lhcb/LHCb/Collision16/BHADRONCOMPLETEEVENT.DST/00069603/0000/00069603_00001488_1.bhadroncompleteevent.dst'],#/eos/lhcb/grid/prod/lhcb/LHCb/Collision16/BHADRONCOMPLETEEVENT.DST/00059907/0001/00059907_00010184_1.bhadroncompleteevent.dst'],#/tmp/dcraik/00042952_00000002_1.ldst'], #/data/dst/MC15.MD.49000004.1.00.dst'],
+#                            clear = True)
+##Type = 'MC'
+
+# Create the generated jets.
+from Configurables import McParticleFlow, McJetBuilder
+genPF = McParticleFlow('genPF')
+genPF.Inputs = [
+    ['PID',        'ban',       '12,-12,14,-14,16,-16'],
+    ['PID',        'particle',  '321,211,130,3222,310,3122,3112,3312,3322,'
+     '-321,-211,-130,-3222,-310,-3122,-3112,-3312,-3322'],
+    ['MCParticle', 'daughters', 'MC/Particles']
+    ]
+genPF.Output = 'Phys/PF/MCParticles'
+genJB = McJetBuilder('genJB')
+genJB.JetPtMin = JetPtMin
+genJB.JetR = 0.5
+genJB.ChrVrt = True
+genJB.NeuVrt = True
+genJB.Inputs = [genPF.Output]
+genJB.Output = 'Phys/JB/MCParticles'
+
+# Create the reconstructed jets.
+from Configurables import HltParticleFlow, HltJetBuilder
+from StandardParticles import (StdLooseKsDD, StdLooseKsLL, StdLooseKsLD,
+                               StdLooseLambdaDD, StdLooseLambdaLL,
+                               StdLooseLambdaLD)
+recPF = HltParticleFlow('recPF')
+recPF.Inputs = [
+    ['Particle',       'particle', StdLooseKsDD.outputLocation()],
+    ['Particle',       'particle', StdLooseKsLL.outputLocation()],
+    ['Particle',       'particle', StdLooseKsLD.outputLocation()],
+    ['Particle',       'particle', StdLooseLambdaDD.outputLocation()],
+    ['Particle',       'particle', StdLooseLambdaLL.outputLocation()],
+    ['Particle',       'particle', StdLooseLambdaLD.outputLocation()],
+    ['ProtoParticle',  'best',     'Rec/ProtoP/Charged'],
+    ['ProtoParticle',  'gamma',    'Rec/ProtoP/Neutrals']
+    ]
+recPF.Output = 'Phys/PF/Particles'
+recPF.ProBestNames = ['mu+', 'e+', 'p+', 'K+', 'pi+']
+recPF.ProBestKeys  = [701,   700,  704,  703,  702]
+recPF.ProBestMins  = [0.5,   0.5,  0.5,  0.5,  0.5]
+recPF.EcalBest = True
+recPF.SprRecover = False
+recPF.TrkLnErrMax = 10
+recPF.TrkUpErrMax = 10
+recPF.TrkDnErrMax = 10
+recJB = HltJetBuilder('recJB')
+recJB.JetEcPath = ''
+recJB.Inputs = [recPF.Output]
+recJB.Output = 'Phys/JB/Particles'
+recJB.JetPtMin = JetPtMin
+
+from commonSelections import *
+
+from PhysSelPython.Wrappers import SelectionSequence
+recSVs_seq = SelectionSequence('recSVs_Seq', TopSelection=recSVs)
+recMus_seq = SelectionSequence('recMus_Seq', TopSelection=recMus)
+
+Jpsi_seq = SelectionSequence('Jpsi_Seq', TopSelection=recJpsi)
+D0_seq = SelectionSequence('D0_Seq', TopSelection=recD0)
+Dp_seq = SelectionSequence('Dp_Seq', TopSelection=recDp)
+Ds_seq = SelectionSequence('Ds_Seq', TopSelection=recDs)
+Lc_seq = SelectionSequence('Lc_Seq', TopSelection=recLc)
+D02K3pi_seq = SelectionSequence('D2K3pi0_Seq', TopSelection=recD02K3pi)
+
+##########################
+
+# Turbo/DaVinci configuration.
+from Configurables import DstConf, TurboConf, DaVinci
+DaVinci().Simulation = False
+DaVinci().Lumi = True
+DaVinci().TupleFile = "LumiTuple.root"
+#DaVinci().appendToMainSequence([genPF, genJB, recPF, recJB])
+DaVinci().appendToMainSequence([recPF, recJB])
+DaVinci().appendToMainSequence([recSVs_seq.sequence(), recMus_seq.sequence()])
+DaVinci().appendToMainSequence([Jpsi_seq.sequence(),D0_seq.sequence(), Dp_seq.sequence(), Ds_seq.sequence(), Lc_seq.sequence(), D02K3pi_seq.sequence()])
+##TODO adding recSVs and recMus changes the daughters of jet objects from smart poniters to Particles
+DaVinci().DataType = '2016'
+DaVinci().EventPreFilters = fltrs.filters ('Filters')
+
+from Configurables import LumiIntegrateFSR, LumiIntegratorConf
+LumiIntegrateFSR('IntegrateBeamCrossing').SubtractBXTypes = ['None']
+
+# Configure the BDT tagger.
+from Configurables import LoKi__BDTTag
+tagger = LoKi__BDTTag()
+tagger.NbvSelect = False
+tagger.Backwards = True
+
+from Configurables import ToolSvc, TriggerTisTos
+for stage in ('Hlt1', 'Hlt2', 'Strip/Phys'):
+    ToolSvc().addTool(TriggerTisTos, stage + "TriggerTisTos")
+    tool = getattr(ToolSvc(), stage + "TriggerTisTos")
+    tool.HltDecReportsLocation = '/Event/' + stage + '/DecReports'
+    tool.HltSelReportsLocation = '/Event/' + stage + '/SelReports'
+
+# Access to classes.
+from collections import OrderedDict
+import ROOT, array, GaudiPython
+from GaudiPython.Bindings import gbl
+STD  = gbl.std
+LHCB = gbl.LHCb
+
+from Ntuple import Ntuple
+
+# GaudiPython configuration.
+gaudi = GaudiPython.AppMgr()
+tes   = gaudi.evtsvc()
+
+# Run.
+import sys, ROOT
+from math import floor
+evtmax = -1
+#try: evtmax = int(sys.argv[1])
+#except: evtmax = float('inf')
+evtnum = 0
+ntuple = Ntuple('output.root', tes, gaudi.toolsvc(), gaudi.detSvc(), recJB.Output, recSVs.outputLocation(), recMus.algorithm().Output)
+while evtmax < 0 or evtnum < evtmax:
+    gaudi.run(1)
+    if not bool(tes['/Event']): break
+    evtnum += 1
+    ntuple.clear()
+
+    # Fill event info.
+    try: ntuple.ntuple['evt_pvr_n'][0] = len(tes['Rec/Vertex/Primary'])
+    except: continue
+
+    # Fill generator level info.
+    fill = False;
+    #gens = tes['MC/Particles']
+    #try:
+    #    ntuple.addGen(gens[0])
+    #    ntuple.addGen(gens[1])
+    #except: pass
+    #try:
+    #    for gen in gens:
+    #        pid = gen.particleID()
+    #        if pid.isHadron() and (pid.hasCharm() or pid.hasBottom()):
+    #            ntuple.addGen(gen)
+    #except: pass
+    #try:
+    #    jets = tes[genJB.Output]
+    #    for jet in jets: ntuple.addGen(jet); fill = True
+    #except: pass
+
+    # Fill reconstructed.
+    try:
+        jets = tes[recJB.Output]
+        ntuple.addTrigger()
+        for jet in jets:
+            ntuple.addJet(jet); fill = True;
+    except: pass
+
+    # fill other tracks
+    try:
+        for trk in tes['Phys/StdAllNoPIDsPions/Particles']:
+            ntuple.addTrk(trk);
+    except: pass
+
+    # fill D's
+    try:
+        jpsis = tes[recJpsi.algorithm().Output]
+        for jpsi in jpsis:
+            ntuple.addDHad(jpsi,"jpsi")
+    except: pass
+    try:
+        d0s = tes[recD0.algorithm().Output]
+        for d0 in d0s:
+            ntuple.addDHad(d0,"d0")
+    except: pass
+    try:
+        dps = tes[recDp.algorithm().Output]
+        for dp in dps:
+            ntuple.addDHad(dp,"dp")
+    except: pass
+    try:
+        dss = tes[recDs.algorithm().Output]
+        for ds in dss:
+            ntuple.addDHad(ds,"ds")
+    except: pass
+    try:
+        lcs = tes[recLc.algorithm().Output]
+        for lc in lcs:
+            ntuple.addDHad(lc,"lc")
+    except: pass
+    try:
+        d0s = tes[recD02K3pi.algorithm().Output]
+        for d0 in d0s:
+            ntuple.addDHad(d0,"k3pi")
+    except: pass
+
+    # Fill the ntuple.
+    if fill: ntuple.fill();
+ntuple.close()
---
+++ jets/davinci/ganga_dijet_back.py
@@ -0,0 +1,76 @@
+import sys
+
+if len(sys.argv)<4:
+    print "Usage: ", sys.argv[0], "<MagUp|MagDown> <2016> <note>"
+    sys.exit()
+polarity = str(sys.argv[1]) #MagDown, MagUp
+year = str(sys.argv[2]) #2015, 2016
+note = str(sys.argv[3])
+
+script=str('Run2Jets_dijet_back.py')
+if year=='2017':
+    script=str('~/git/lhcb/jets/davinci/Run2Jets_dijet17_back.py')
+if year=='2018':
+    script=str('~/git/lhcb/jets/davinci/Run2Jets_dijet18_back.py')
+
+job_name = "RIIJ_dijet_back" +str(note) + str(polarity) + str(year)
+print job_name
+print script
+
+#DV = GaudiExec(directory="~/DaVinciDev_v42r6p1")
+DV = GaudiExec(directory="~/DaVinciDev_v44r9")
+DV.options = [script]
+DV.useGaudiRun = False
+BK_locations = []
+
+if year=='2016':
+    BK_locations = ['/LHCb/Collision16/Beam6500GeV-VeloClosed-'+polarity+'/Real Data/Reco16/Stripping28r1/90000000/BHADRONCOMPLETEEVENT.DST']
+elif year=='2017':
+    BK_locations = ['/LHCb/Collision17/Beam6500GeV-VeloClosed-'+polarity+'/Real Data/Reco17/Stripping29r2/90000000/BHADRONCOMPLETEEVENT.DST']
+elif year=='2018':
+    BK_locations = ['/LHCb/Collision18/Beam6500GeV-VeloClosed-'+polarity+'/Real Data/Reco18/Stripping34/90000000/BHADRONCOMPLETEEVENT.DST']
+else:
+    sys.exit()
+data = LHCbDataset()
+bk = BKQuery()
+
+if note not in ["test","testgrid"]:
+    for path in BK_locations:
+        bk.path = path
+        tmp = bk.getDataset()
+        print path, len(tmp.files)
+        if len(tmp.files) > 0:
+            data.extend( tmp )
+
+    if len(data.files) < 1:
+        sys.exit()
+
+
+j = Job(
+  name           = job_name,
+  application    = DV,
+  inputdata      = data,
+  #do_auto_resubmit = True,
+  inputfiles = [LocalFile('commonSelections.py'), LocalFile('Ntuple.py')]
+  )
+
+if note=="test":
+    j.splitter       = SplitByFiles(filesPerJob = 1, maxFiles = 4)
+    j.backend        = Local()
+    j.outputfiles     = [LocalFile("*.root")]
+elif note=="testgrid":
+    j.splitter       = SplitByFiles(filesPerJob = 25, maxFiles = 10, ignoremissing=True)
+    j.backend        = Dirac()
+    j.outputfiles     = [LocalFile("*.root")]
+else:
+    j.splitter       = SplitByFiles(filesPerJob = 25, maxFiles = -1, ignoremissing=True)
+    j.backend        = Dirac()
+    j.outputfiles    = [DiracFile("*.root")]
+
+if note in ["test","testgrid"]:
+        j.inputdata = LHCbDataset(['LFN:/lhcb/LHCb/Collision16/BHADRONCOMPLETEEVENT.DST/00069603/0000/00069603_00001488_1.bhadroncompleteevent.dst'])
+
+j.parallel_submit = True
+
+queues.add(j.submit)
+
